be117c9aea0949cafa5083fa8a51810242d62fb1
Chau Vuong Vo <chauvuong223068@gmail.com>
2022-06-22 05:36:35 +0700

    initial commit

---
+++ 2-coffee-website/js/script.js
@@ -0,0 +1,86 @@
+// Swiper js
+var swiper = new Swiper(".mySwiper", {
+  slidesPreview: 1,
+  // grabCursor: true,
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+// Nav open close
+const body = document.querySelector("body"),
+  navMenu = body.querySelector(".menu-content"),
+  navOpenBtn = body.querySelector(".nav-open-btn"),
+  navCloseBtn = navMenu.querySelector(".nav-close-btn");
+
+if (navMenu && navOpenBtn) {
+  navOpenBtn.addEventListener("click", () => {
+    navMenu.classList.add("open");
+    body.style.overflowY = "hidden";
+  });
+}
+if (navMenu && navCloseBtn) {
+  navCloseBtn.addEventListener("click", () => {
+    navMenu.classList.remove("open");
+    body.style.overflowY = "scroll";
+  });
+}
+// Change header bg color
+window.addEventListener("scroll", () => {
+  const scrollY = window.pageYOffset;
+
+  if (scrollY > 5) {
+    document.querySelector("header").classList.add("header-active");
+  } else {
+    document.querySelector("header").classList.remove("header-active");
+  }
+
+  // Scroll up button
+  const scrollUpBtn = document.querySelector(".scrollUp-btn");
+
+  if (scrollY > 250) {
+    scrollUpBtn.classList.add("scrollUpBtn-active");
+  } else {
+    scrollUpBtn.classList.remove("scrollUpBtn-active");
+  }
+
+  // Nav link indicator
+  const sections = document.querySelectorAll("section[id]");
+
+  sections.forEach((section) => {
+    const sectionHeight = section.offsetHeight;
+    const sectionTop = section.offsetTop - 60;
+
+    let navId = document.querySelector(`.menu-content a[href*= ${section.id}]`);
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      navId.classList.add("active-navlink");
+    } else {
+      navId.classList.remove("active-navlink");
+    }
+
+    navId.addEventListener("click", () => {
+      navMenu.classList.remove("open");
+      body.style.overflowY = "scroll";
+    });
+  });
+});
+
+// Scroll Reveal Animation
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+});
+sr.reveal(
+  `.section-subtitle, .section-title, .section-description, .brand-image, .testimonial, .logo-content, .newsletter-inputBox, .newsletter-mediaIcon, .footer-content, .footer-links`,
+  { interval: 100 }
+);
+sr.reveal(`.about-imageContent, .menu-items`, { origin: "left" });
+sr.reveal(`.about-details, .time-table`, { origin: "right" });
